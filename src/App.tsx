b324ff48b37e99a9ada05f71292df0a1f017f03f
Linh <linh.lv@zetagroup.vn>
2023-03-28 14:59:47 +0700

    demo

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useEffect } from 'react';
+import AppRoutes from './components/routes/AppRoutes';
+import FallbackComponent from './components/errorFallback/FallbackComponent';
+import { ErrorBoundary } from 'react-error-boundary';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from './hooks';
+import { authArgs, authorizeUser } from './store/reducers/auth/actionCreators';
+
+const App: FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      const args: authArgs = { type: 'checkAuth' };
+      dispatch(authorizeUser(args));
+    }
+  }, [dispatch]);
+
+  return (
+    <ErrorBoundary
+      FallbackComponent={FallbackComponent}
+      onReset={() => navigate('/')}
+    >
+      <AppRoutes />
+    </ErrorBoundary>
+  );
+};
+
+export default App;
